8c4fecaae8ce2cc42de49eb84b01ade1d87523fb
vignesh <95468303+vigneshvvv@users.noreply.github.com>
2025-12-29 17:55:05 +0530

    Create forloop.py

---
+++ forloop.py
@@ -0,0 +1,27 @@
+
+num = 5
+
+#Example for giving only end index
+# for i in range(num):
+#     print(i)
+
+#Example for giving starting and ending range
+# for i in range(1 , 50):
+#     print(i)   
+
+#Example for giving starting and ending index incremented by 2
+# for i in range(1, 50, 2):
+#     print(i)
+
+
+#Decrementing the index by 1
+# for i in range(50, 1, -1):
+#     print(i)   
+
+num1 = 3
+
+#while loop example
+while(num1 <5):
+    # num = num +1
+    print(num1)
+    num1 += 1
