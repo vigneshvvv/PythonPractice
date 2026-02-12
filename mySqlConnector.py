4c5f3ac6742aab1840e3e3f61484b3bd5b2d244d
vignesh <95468303+vigneshvvv@users.noreply.github.com>
2026-02-12 17:34:07 +0530

    Create mySqlConnector.py

---
+++ mySqlConnector.py
@@ -0,0 +1,30 @@
+import mysql.connector
+from mysql.connector import Error
+
+def mySqlConnect():
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host = "jdbc:mysql://localhost:3306",
+            database = "javatrainingpro",
+            user = "root",
+            password = "Vignesh333#"
+        )
+
+        if connection.is_connected():
+            print("connection established")
+            cursor = connection.cursor()
+            selectQuery = "select * from books"
+            cursor.execute(selectQuery)
+            result = cursor.fetchall()
+            print(result)
+    except Error as e:
+        print(e)
+    finally:
+        # if connection.is_connected():
+            # cursor.close()
+            # connection.close()    
+        print("finally executed")    
+
+
+mySqlConnect()
